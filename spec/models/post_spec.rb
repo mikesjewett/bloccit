8081cc12728257ca8df268846b8fefc088e596a0
Mike Jewett <mikesjewett@gmail.com>
2015-07-20 15:02:48 -0400

    Completed Models

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  context "attributes" do
+    let(:post) { Post.new(title: "New Post Title", body: "New Post Body") }
+
+    it "should respond to title" do
+      expect(post).to respond_to(:title)
+    end
+
+    it "should respond to body" do
+      expect(post).to respond_to(:body)
+    end
+  end
+end
