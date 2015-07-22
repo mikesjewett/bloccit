c7f934d5fec1bcf10a80d7dfb51b7b2942f9f5b0
Mike Jewett <mikesjewett@gmail.com>
2015-07-22 12:42:47 -0400

    Completed seeds

---
+++ lib/random_data.rb
@@ -0,0 +1,26 @@
+module RandomData
+  def random_paragraph
+    sentences = []
+    rand(4..6).times do
+      sentences << random_sentence
+    end
+
+    sentences = sentences.join(" ")
+  end
+
+  def random_sentence
+    strings = []
+    rand(1..8).times do
+      strings << random_string
+    end
+
+    sentence = strings.join(" ")
+    sentence.capitalize << "."
+  end
+
+  def random_string
+    letters = ('a'..'z').to_a
+    letters.shuffle!
+    letters[0,rand(1..8)].join
+  end
+end
