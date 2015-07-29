baebdc3fa3cf3bb0d5337e2efeb80721b874fa0f
Mike Jewett <mikesjewett@gmail.com>
2015-07-29 18:07:43 -0400

    Completed Validations

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,8 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
 
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:body) }
+  it { should validate_presence_of(:topic) }
+  it { should validate_length_of(:title).is_at_least(5) }
+  it { should validate_length_of(:body).is_at_least(20) }
   it { should belong_to(:topic) }
-  
+
   context "attributes" do
     let(:post) { Post.new(title: "New Post Title", body: "New Post Body") }
 
