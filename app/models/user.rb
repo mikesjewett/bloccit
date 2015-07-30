bef24e1e7a3f585849afaf24cf7aebddbdccc84a
Mike Jewett <mikesjewett@gmail.com>
2015-07-30 18:37:55 -0400

    Completed Authentication User Model

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+  before_save { self.email = email.downcase }
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :name, length: { minimum: 1, maximum: 100 }, presence: true
+  validates :email,
+    presence: true,
+    uniqueness: { case_sensitive: false },
+    length: { minimum: 3, maximum: 100 },
+    format: { with: EMAIL_REGEX }
+  validates :password, presence: true, length: { minimum: 6 }
+  has_secure_password
+end
