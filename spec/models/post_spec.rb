b0a9c6370bdd5d0cf895b6f0134534037f061a4c
Mike Jewett <mikesjewett@gmail.com>
2015-08-04 13:44:52 -0400

    Completed Topics and Post 2

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,9 +1,15 @@
 require 'rails_helper'
+include RandomData
 
 RSpec.describe Post, type: :model do
-  context "attributes" do
-    let(:post) { Post.new(title: "New Post Title", body: "New Post Body") }
 
+  let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
+
+  let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
+
+  it { should belong_to(:topic) }
+
+  context "attributes" do
     it "should respond to title" do
       expect(post).to respond_to(:title)
     end
