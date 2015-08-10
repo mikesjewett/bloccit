eea06172cbd1e7057edb18e951ba13c10bd7c633
Mike Jewett <mikesjewett@gmail.com>
2015-08-10 14:20:08 -0400

    Fix tests

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,5 @@
 class PostsController < ApplicationController
-  def index
-    @posts = Post.all
-  end
-
   def show
-    @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:id])
   end
 
@@ -14,13 +9,15 @@ class PostsController < ApplicationController
   end
 
   def create
-    @topic = Topic.find(params[:topic_id])
-    @post = Post.new(params.require(:post).permit(:title, :body))
-    @post.topic = @topic
+    @post = Post.new
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
+    topic = Topic.find(params[:topic_id])
+    @post.topic = topic
 
     if @post.save
       flash[:notice] = "Post was saved."
-      redirect_to [@topic, @post]
+      redirect_to [topic, @post]
     else
       flash[:error] = "There was an error saving the post. Please try again."
       render :new
@@ -28,19 +25,17 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:id])
   end
 
   def update
-    @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:id])
     @post.title = params[:post][:title]
     @post.body = params[:post][:body]
 
     if @post.save
       flash[:notice] = "Post was updated."
-      redirect_to [@topic, @post]
+      redirect_to [@post.topic, @post]
     else
       flash[:error] = "There was an error saving the post. Please try again."
       render :edit
@@ -48,12 +43,11 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:id])
 
     if @post.destroy
       flash[:notice] = "\"#{@post.title}\" was deleted successfully."
-      redirect_to @topic
+      redirect_to @post.topic
     else
       flash[:error] = "There was an error deleting the post."
       render :show
